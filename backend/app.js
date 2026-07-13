0c847388602166efdc7e302c1ce3617cf5e4955b
Sijan Kunwar <np03cs4s250067@heraldcollege.edu.np>
2026-07-13 10:52:22 +0545

    Cors and dotenv modules added

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,19 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 import carRoutes from './src/routes/carRoutes.js'
 import bookingRoutes from "./src/routes/bookingRoutes.js";
 
 
 const app = express()
+
 app.use(express.json())
-const PORT = 3001
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
 
 app.use(carRoutes);
 app.use(bookingRoutes);
