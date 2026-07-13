e50d811689c938718fc563862ff34efa5172510b
Sijan Kunwar <np03cs4s250067@heraldcollege.edu.np>
2026-07-13 12:05:04 +0545

    Connected the system to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,13 @@
 import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-
-dotenv.config()
-
+import cors from 'cors'
+import dotenv from 'dotenv'
+import dbConnection from './src/config/db.js'
 import carRoutes from './src/routes/carRoutes.js'
 import bookingRoutes from "./src/routes/bookingRoutes.js";
 
 
 const app = express()
+dotenv.config()
 
 app.use(express.json())
 app.use(cors())
@@ -17,6 +16,8 @@ const PORT = process.env.PORT || 3000
 
 app.use(carRoutes);
 app.use(bookingRoutes);
+
+await dbConnection()
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
---
+++ backend/src/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path:'../../.env'
+})
+
+const dbConnection = async()=> {
+    try{
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("MongoDB successfully connected")
+    }catch(error){
+        console.log(`MongoDB cannot be connected ${error.message}`)
+        process.exit()
+    }
+}
+
+export default dbConnection
